42aca804265987f9372bab9a95ec35e1b9831d27
Morgan Hondros <morganhondros@gmail.com>
2019-06-29 15:18:49 -0400

    VCs added. Outlets linked to Add ThoughtVC. textViewDidBeginEditing Added for placeholder functionality.

---
+++ RNDM/Controller/AddThoughVC.swift
@@ -0,0 +1,42 @@
+//
+//  AddThoughVCViewController.swift
+//  RNDM
+//
+//  Created by Morgan Hondros on 6/29/19.
+//  Copyright © 2019 Morgan Hondros. All rights reserved.
+//
+
+import UIKit
+
+class AddThoughVC: UIViewController, UITextViewDelegate {
+
+    @IBOutlet weak var categorySegment: UISegmentedControl!
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var thoughtTxt: UITextView!
+    @IBOutlet weak var postBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        postBtn.layer.cornerRadius = 4
+        thoughtTxt.layer.cornerRadius = 4
+        thoughtTxt.text = "My random thought..."
+        thoughtTxt.textColor = UIColor.lightGray
+        thoughtTxt.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+        textView.textColor = UIColor.darkGray
+    }
+    
+    @IBAction func categoryChanged(_ sender: Any) {
+    }
+    @IBAction func postBtnTapped(_ sender: Any) {
+    }
+    
+
+}
