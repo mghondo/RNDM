29db3b7aeacd6449e6d231f976b542ee281975cf
Morgan Hondros <morganhondros@gmail.com>
2019-07-03 09:03:03 -0400

    Create user functionality setup. Not tested yet as navigation controller is still pointed at the blog after the login.

--- RNDM/Controller/CreateUserVC.swift
+++ RNDM/Controller/CreateUserVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class CreateUserVC: UIViewController {
     
@@ -16,23 +18,78 @@ class CreateUserVC: UIViewController {
     @IBOutlet weak var createBtn: UIButton!
     @IBOutlet weak var cancelBtn: UIButton!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         createBtn.layer.cornerRadius = 10
         cancelBtn.layer.cornerRadius = 10
-
+        
         
     }
     
     @IBAction func createTapped(_ sender: Any) {
         
+        guard let email = emailTxt.text,
+            let password = passwordTxt.text,
+            let username = usernameTxt.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                debugPrint("TOTALLY AND ERRO CREATING USER: \(error.localizedDescription)")
+            }
+            let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+            changeRequest?.displayName = username
+            changeRequest?.commitChanges(completion: { (error) in
+                if let error = error {
+                    debugPrint(error.localizedDescription)
+                }
+            })
+            
+            guard let userId = user?.user.uid else { return }
+            Firestore.firestore().collection(USERS_REF).document(userId).setData(
+                [USERNAME : username,
+                 DATE_CREATED : FieldValue.serverTimestamp()], completion: { error in
+                    if let error = error {
+                        print(#line, #function, error.localizedDescription)
+                    } else {
+                        self.dismiss(animated: true, completion: nil)
+                    }
+            })
+
+        }
     }
     
     @IBAction func cancelTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
     
-
+//    Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+//    if let error = error {
+//    debugPrint("Error creating user: \(error.localizedDescription)")
+//    }
+//
+//    let changeRequest = CURRENT_USER?.createProfileChangeRequest()
+//    changeRequest?.displayName = username
+//    changeRequest?.commitChanges(completion: { (error) in
+//    if let error = error {
+//    debugPrint(error.localizedDescription)
+//    }
+//    })
+//
+//    guard let userId = CURRENT_USER?.uid else { return }
+//    Firestore.firestore().collection(USERS_REF).document(userId).setData([
+//    USERNAME : username,
+//    DATE_CREATED : FieldValue.serverTimestamp()
+//    ], completion: { (error) in
+//    if let error = error {
+//    debugPrint(error.localizedDescription)
+//    } else {
+//    self.dismiss(animated: true, completion: nil)
+//    }
+//    })
+//
+    
+    
 }
--- RNDM/Utilities/Constants.swift
+++ RNDM/Utilities/Constants.swift
@@ -9,9 +9,13 @@
 import Foundation
 
 let THOUGHTS_REF = "thoughts"
+let USERS_REF = "users"
+let CURRENT_USER = "Auth.auth().currentUser"
+
 let CATEGORY = "category"
 let NUM_COMMENTS = "numComments"
 let NUM_LIKES = "numLikes"
 let THOUGHT_TXT = "thoughtTxt"
 let TIMESTAMP = "timestamp"
 let USERNAME = "username"
+let DATE_CREATED = "dataCreated"
